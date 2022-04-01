59486c2cb22c670d56b1cfa3615e48872c9bfb5e
DragonWF <plarisanmarc@gmail.com>
2022-04-01 19:41:24 +0800

    Added functionality for changing the nav icon when clicked

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,18 @@
 $(document).ready(() => {
   const menuToggle = $(".toggle");
   const showcase = $(".home-content");
+  const navIcon = $("#navIcon");
+  let menuOpened = false;
 
   menuToggle.click(() => {
+    menuOpened = menuOpened ? false : true;
     menuToggle.toggleClass("active");
     showcase.toggleClass("active");
+
+    if (menuOpened) {
+      navIcon.removeClass("fa-bars").addClass("fa-xmark");
+    } else {
+      navIcon.removeClass("fa-xmark").addClass("fa-bars");
+    }
   });
 });
