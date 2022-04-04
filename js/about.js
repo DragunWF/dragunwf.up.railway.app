64225b83b82a66410ef0ba86eb32ce11a569d937
DragonWF <plarisanmarc@gmail.com>
2022-04-05 01:59:02 +0800

    Removed onPageLoad() function

--- js/about.js
+++ js/about.js
@@ -15,12 +15,6 @@ $(document).ready(() => {
     currentId = `#p${infoIndex}`;
   }
 
-  function onPageLoad() {
-    const hiddenContents = ["#about-header", "#p0", ".about-buttons"];
-    for (let element of hiddenContents) $(element).hide();
-    for (let content of hiddenContents) $(content).fadeIn(850);
-  }
-
   next.click(() => {
     infoIndex++;
     updateAboutText();
@@ -30,6 +24,4 @@ $(document).ready(() => {
     infoIndex--;
     updateAboutText();
   });
-
-  onPageLoad();
 });
