a2f999bb50301aece94c38734f71b79f597424aa
DragonWF <plarisanmarc@gmail.com>
2022-04-04 23:05:32 +0800

    Split code into two files (Home and about page functionality)

---
+++ js/about.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  const next = $("#next");
+  const back = $("#back");
+  let currentId = "#p0";
+  let infoIndex = 0;
+
+  function updateAboutText() {
+    const indexLimit = 4;
+    if (infoIndex > indexLimit) infoIndex = 0;
+    else if (infoIndex < 0) infoIndex = indexLimit;
+
+    $(currentId).fadeOut(350, () => {
+      $(`#p${infoIndex}`).fadeIn(350);
+    });
+    currentId = `#p${infoIndex}`;
+  }
+
+  next.click(() => {
+    infoIndex++;
+    updateAboutText();
+  });
+
+  back.click(() => {
+    infoIndex--;
+    updateAboutText();
+  });
+});
