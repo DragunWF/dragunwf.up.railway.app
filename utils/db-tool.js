152b8eb34155b429e8d0918b6912f34e051c9cad
DragonWF <plarisanmarc@gmail.com>
2022-04-08 00:42:55 +0800

    Added boiler code

---
+++ utils/db-tool.js
@@ -0,0 +1,15 @@
+import mysql from "mysql";
+
+const db = mysql.createConnection();
+
+class DatabaseTool {
+  static insertPageVisit(name, path) {
+    return;
+  }
+
+  static insertSocialsVisit(name) {
+    return;
+  }
+}
+
+export default DatabaseTool;
