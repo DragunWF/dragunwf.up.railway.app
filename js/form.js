13d17be1d5e0bf762d198d64be8d07a190b8fedb
DragonWF <plarisanmarc@gmail.com>
2022-04-07 16:10:55 +0800

    Added main logic for form and validation

--- js/form.js
+++ js/form.js
@@ -3,4 +3,27 @@ $(document).ready(() => {
   const reset = $("#reset");
   const formName = $("#formName");
   const formTextArea = $("#formTextArea");
+
+  submit.click(() => {
+    const messageLength = formTextArea.val().length;
+    const nameLength = formName.val().length;
+
+    if (!messageLength) 
+      alert("Your message is empty!");
+    else if (messageLength < 5) 
+      alert("Your message is too short!");
+    else if (messageLength > 2000)
+      alert("Your message must be below 2000 characters!");
+    else if (nameLength > 50) 
+      alert("Your name must be below 50 characters!");
+    else {
+      if (!nameLength) formName.val("Anonymous");
+      submit.attr("type", "submit");
+    }
+  });
+
+  reset.click(() => {
+    formName.val("");
+    formTextArea.val("");
+  });
 });
