e26811b738c37e6c04c2dca79592b41d16ed032d
DragonWF <plarisanmarc@gmail.com>
2022-04-08 02:54:17 +0800

    Added connection config, connect function, insert functions

--- utils/db-tool.js
+++ utils/db-tool.js
@@ -1,14 +1,34 @@
+import "dotenv/config";
 import mysql from "mysql";
 
-const db = mysql.createConnection();
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+db.connect((err) => {
+  if (err) console.error(err);
+});
 
 class DatabaseTool {
-  static insertPageVisit(name, path) {
-    return;
+  static insertPageVisit(pageId) {
+    try {
+      const sqlQuery = `INSERT INTO pages (page_id, date, time) VALUES (${pageId}, );`;
+      db.query(sqlQuery);
+    } catch (err) {
+      console.error(err);
+    }
   }
 
-  static insertSocialsVisit(name) {
-    return;
+  static insertSocialsVisit(linkId) {
+    try {
+      const sqlQuery = `INSERT INTO link_clicks (name, date, time) VALUES (${linkId}, );`;
+      db.query(sqlQuery);
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
