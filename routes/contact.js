ce3a62c1fef2ee20e6cb808ffd2bf63c48f0c5d9
DragonWF <plarisanmarc@gmail.com>
2022-04-07 19:22:49 +0800

    Fixed rendering

--- routes/contact.js
+++ routes/contact.js
@@ -3,11 +3,11 @@ import express from "express";
 const contactRouter = express.Router();
 
 contactRouter.get("/", (req, res) => {
-  res.render("skills");
+  res.render("contact");
 });
 
 contactRouter.get("/form", (req, res) => {
-  res.render("skills-info");
+  res.render("message-form");
 });
 
 export default contactRouter;
