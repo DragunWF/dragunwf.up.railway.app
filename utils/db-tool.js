d6fb26c6181d9f848286e8662a7bb6e69fa8ba3c
DragonWF <plarisanmarc@gmail.com>
2022-04-08 13:36:29 +0800

    Implemented error handling

--- utils/db-tool.js
+++ utils/db-tool.js
@@ -1,33 +1,44 @@
 import "dotenv/config";
 import mysql from "mysql";
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+let databaseConnected = false;
+try {
+  const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
 
-db.connect((err) => {
-  if (err) console.error(err);
-});
+  db.connect((err) => {
+    if (err) console.error(err);
+  });
+
+  databaseConnected = true;
+} catch (err) {
+  console.error(err);
+}
 
 class DatabaseTool {
   static insertPageVisit(pageId) {
-    try {
-      const sqlQuery = `INSERT INTO pages (page_id, date, time) VALUES (${pageId}, );`;
-      db.query(sqlQuery);
-    } catch (err) {
-      console.error(err);
+    if (databaseConnected) {
+      try {
+        const sqlQuery = `INSERT INTO pages (page_id, date, time) VALUES (${pageId}, CURDATE(), CURTIME());`;
+        db.query(sqlQuery);
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
 
   static insertSocialsVisit(linkId) {
-    try {
-      const sqlQuery = `INSERT INTO link_clicks (name, date, time) VALUES (${linkId}, );`;
-      db.query(sqlQuery);
-    } catch (err) {
-      console.error(err);
+    if (databaseConnected) {
+      try {
+        const sqlQuery = `INSERT INTO link_clicks (name, date, time) VALUES (${linkId}, CURDATE(), CURTIME());`;
+        db.query(sqlQuery);
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
 }
