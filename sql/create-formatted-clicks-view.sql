2bc0a4880748d9517bfdd266c306aa61c974318e
DragonWF <plarisanmarc@gmail.com>
2022-04-08 16:25:28 +0800

    Minor name changes

--- sql/create-formatted-clicks-view.sql
+++ sql/create-formatted-clicks-view.sql
@@ -6,7 +6,7 @@ SELECT
     sl.link AS link_clicked,
 	DATE_FORMAT(lc.date, "%M %d %Y") AS date_of_click,
     DATE_FORMAT(lc.hour, "%H:%i %p") AS hour_of_click,
-    sl.name AS socials_visited
+    sl.name AS social_visited
 FROM link_clicks lc
 JOIN social_links sl USING (link_id)
 ORDER BY click_id;
