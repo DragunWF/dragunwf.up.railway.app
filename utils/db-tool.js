1e9b5c6f505b8f7e7f4fef84e3e806e8e7aae986
DragonWF <plarisanmarc@gmail.com>
2022-04-11 19:52:16 +0800

    Replaced traditional connections with connection pools

--- utils/db-tool.js
+++ utils/db-tool.js
@@ -1,23 +1,15 @@
 import "dotenv/config";
 import mysql from "mysql";
 
-let databaseConnected = false;
-const db = mysql.createConnection({
+// let databaseConnected = false;
+const db = mysql.createPool({
+  connectionLimit: 5,
+  aquireTimeout: 10000,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
-
-try {
-  db.connect((err) => {
-    if (err) console.error(err);
-  });
-  databaseConnected = true;
-  console.log("Database connected successfully!");
-} catch (err) {
-  console.error(err);
-}
 
 // My timezone is GMT+8. MySQL by default uses UTC time when outputing CURTIME() or CURDATE()
 const myTime = 'ADDTIME(CURTIME(), "08:00")';
@@ -25,25 +17,17 @@ const myDate = 'ADDTIME(NOW(), "08:00")';
 
 class DatabaseTool {
   static insertPageVisit(pageId) {
-    if (databaseConnected) {
-      try {
-        const sqlQuery = `INSERT INTO visits (page_id, date, hour) VALUES (${pageId}, ${myDate}, ${myTime});`;
-        db.query(sqlQuery);
-      } catch (err) {
-        console.error(err);
-      }
-    }
+    const sqlQuery = `INSERT INTO visits (page_id, date, hour) VALUES (${pageId}, ${myDate}, ${myTime});`;
+    db.query(sqlQuery, (err, results) => {
+      if (err) console.error(err);
+    });
   }
 
   static insertSocialsVisit(linkId) {
-    if (databaseConnected) {
-      try {
-        const sqlQuery = `INSERT INTO link_clicks (link_id, date, hour) VALUES (${linkId}, ${myDate}, ${myTime});`;
-        db.query(sqlQuery);
-      } catch (err) {
-        console.error(err);
-      }
-    }
+    const sqlQuery = `INSERT INTO link_clicks (link_id, date, hour) VALUES (${linkId}, ${myDate}, ${myTime});`;
+    db.query(sqlQuery, (err, results) => {
+      if (err) console.error(err);
+    });
   }
 }
 
