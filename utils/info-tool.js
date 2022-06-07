db52ff2161879990866099ec9357ee7ee6b3fc39
DragonWF <plarisanmarc@gmail.com>
2022-06-07 18:02:57 +0800

    Implemented getter methods for page ids and social ids

--- utils/info-tool.js
+++ utils/info-tool.js
@@ -1,3 +1,30 @@
-class InfoTool {}
+class InfoTool {
+  static #pageIds = {
+    home: 1,
+    about: 2,
+    projects: 3,
+    projectsList: 4,
+    skills: 5,
+    skillsInfo: 6,
+    contact: 7,
+    messageForm: 8,
+  };
+  static socialIds = {
+    github: 1,
+    itch: 2,
+    steam: 3,
+    codewars: 4,
+    typeracer: 5,
+    discord: 6,
+  };
+
+  static getPageId(pageName) {
+    return this.#pageIds[pageName];
+  }
+
+  static getSocialId(socialName) {
+    return this.#socialIds[socialName];
+  }
+}
 
 export default InfoTool;
