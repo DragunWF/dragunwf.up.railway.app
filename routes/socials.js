041fd91bad2300c9d056bda2a3af0e2c0bd312cf
DragonWF <plarisanmarc@gmail.com>
2022-06-07 18:08:07 +0800

    Implemented info tool for social IDs

--- routes/socials.js
+++ routes/socials.js
@@ -1,5 +1,6 @@
 import express from "express";
 import DatabaseTool from "../utils/db-tool.js";
+import InfoTool from "../utils/info-tool.js";
 
 const socialsRouter = express.Router();
 
@@ -8,32 +9,32 @@ socialsRouter.get("/", (req, res) => {
 });
 
 socialsRouter.get("/github", (req, res) => {
-  DatabaseTool.insertSocialsVisit(1);
+  DatabaseTool.insertSocialsVisit(InfoTool.getSocialId("github"));
   res.redirect("https://github.com/DragunWF");
 });
 
 socialsRouter.get("/itch.io", (req, res) => {
-  DatabaseTool.insertSocialsVisit(2);
+  DatabaseTool.insertSocialsVisit(InfoTool.getSocialId("itch"));
   res.redirect("https://dragonwf.itch.io/");
 });
 
 socialsRouter.get("/steam", (req, res) => {
-  DatabaseTool.insertSocialsVisit(3);
+  DatabaseTool.insertSocialsVisit(InfoTool.getSocialId("steam"));
   res.redirect("https://steamcommunity.com/profiles/76561198201145658");
 });
 
 socialsRouter.get("/codewars", (req, res) => {
-  DatabaseTool.insertSocialsVisit(4);
+  DatabaseTool.insertSocialsVisit(InfoTool.getSocialId("codewars"));
   res.redirect("https://www.codewars.com/users/DragonWF");
 });
 
 socialsRouter.get("/typeracer", (req, res) => {
-  DatabaseTool.insertSocialsVisit(5);
+  DatabaseTool.insertSocialsVisit(InfoTool.getSocialId("typeracer"));
   res.redirect("https://data.typeracer.com/pit/profile?user=dragonwf");
 });
 
 socialsRouter.get("/discord", (req, res) => {
-  DatabaseTool.insertSocialsVisit(6);
+  DatabaseTool.insertSocialsVisit(InfoTool.getSocialId("discord"));
   res.redirect("https://discord.gg/9JdnnPN");
 });
 
