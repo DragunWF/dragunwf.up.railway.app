85a24386bda24f3e1ae572c892fdd0db2335eb38
DragonWF <plarisanmarc@gmail.com>
2022-06-08 17:03:52 +0800

    Fixed bug and passed in languages array to skills page

--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,20 @@
 import express from "express";
 import DatabaseTool from "../utils/db-tool.js";
 import InfoTool from "../utils/info-tool.js";
+import GeneralTool from "../utils/general-tool.js";
 
 const skillsRouter = express.Router();
 
 skillsRouter.get("/", (req, res) => {
-  res.render("skills");
+  const languagesArray = GeneralTool.splitAmpersandsFromArray(
+    InfoTool.getSkillsList("languages")
+  );
+  res.render("skills", {
+    data: {
+      languages: GeneralTool.concatenateStringsWithAnd(languagesArray),
+    },
+  });
+
   DatabaseTool.insertPageVisit(InfoTool.getPageId("skills"));
 });
 
@@ -15,11 +24,10 @@ skillsRouter.get("/more", (req, res) => {
       languages: InfoTool.getSkillsList("languages"),
       misc: InfoTool.getSkillsList("misc"),
       technologies: InfoTool.getSkillsList("technologies"),
-      concatenateSkills: (skills) => {
-        return;
-      },
+      concatenateSkills: GeneralTool.concatenateStrings,
     },
   });
+
   DatabaseTool.insertPageVisit(InfoTool.getPageId("skillsInfo"));
 });
 
