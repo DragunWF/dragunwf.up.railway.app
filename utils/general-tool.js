b01bb7512168f7adcbb067bbde4c6ef96e7fdd17
DragonWF <plarisanmarc@gmail.com>
2022-06-08 15:17:10 +0800

    Implemented string concatenation methods

--- utils/general-tool.js
+++ utils/general-tool.js
@@ -1,3 +1,13 @@
 class GeneralTool {
-  static concatenateSkills() {}
+  static concatenateStrings(array) {
+    return array.join(", ");
+  }
+
+  static concatenateStringsWithAnd(array) {
+    const lastItem = array[array.length - 1];
+    array.pop();
+    return `${array.join(", ")}, and ${lastItem}`;
+  }
 }
+
+export default GeneralTool;
