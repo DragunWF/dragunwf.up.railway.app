85cbae2aec87fea12a6118d66914b6a449f53686
DragonWF <plarisanmarc@gmail.com>
2022-06-09 20:19:16 +0800

    Fixed table names and changed date function

--- sql/events/create-monthly-visit-logs-event.sql
+++ sql/events/create-monthly-visit-logs-event.sql
@@ -6,9 +6,9 @@ ON SCHEDULE
 	EVERY 1 MONTH STARTS "2022-01-01"
 DO BEGIN
 	INSERT INTO monthly_visits (page_visits, link_visits, date)
-    VALUES ((SELECT COUNT(*) FROM page_visits),
+    VALUES ((SELECT COUNT(*) FROM visits),
 			(SELECT COUNT(*) FROM link_clicks),
-             DATE_FORMAT(DATE_SUB(CURTIME(), INTERVAL 1 MONTH), "%M %Y"));
+             DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), "%M %Y"));
             
     DELETE FROM visits;
     DELETE FROM link_clicks;
