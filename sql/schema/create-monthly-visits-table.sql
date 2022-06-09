ae5d4c0bf68993e50798b13e1f20a24c3880c73f
DragonWF <plarisanmarc@gmail.com>
2022-06-09 18:55:28 +0800

    Added create-monthly-visits table

---
+++ sql/schema/create-monthly-visits-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `dragunwf_app_db`.`monthly_visits` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `page_visits` INT NOT NULL,
+  `link_visits` INT NOT NULL,
+  `date` DATE NOT NULL,
+  PRIMARY KEY (`id`));
