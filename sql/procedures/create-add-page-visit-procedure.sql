1cea5bcd23e122b6a11c005335aa2c4f5b436b96
DragonWF <plarisanmarc@gmail.com>
2022-06-09 18:22:13 +0800

    Created add_page_visit stored procedure (sql)

---
+++ sql/procedures/create-add-page-visit-procedure.sql
@@ -0,0 +1,13 @@
+DROP PROCEDURE IF EXISTS add_page_visit;
+
+DELIMITER $$
+CREATE PROCEDURE add_page_visit
+(
+	visited_page_id INT
+)
+BEGIN
+	INSERT INTO visits (page_id, date, hour)
+    VALUES (visited_page_id, CURTIME(), NOW(); 
+END $$
+
+DELIMITER ;
