110a8e1e810b075b7aa13961bce110e4efd976f4
DragonWF <plarisanmarc@gmail.com>
2022-06-09 18:31:44 +0800

    Changed sql queries to implement stored procedures (calls)

--- utils/db-tool.js
+++ utils/db-tool.js
@@ -11,28 +11,23 @@ const db = mysql.createPool({
   database: process.env.DB_DATABASE,
 });
 
-// My timezone is GMT+8. MySQL by default uses UTC time when outputing CURTIME() or CURDATE()
-// I won't be using these constant variables right now
-const myTime = 'ADDTIME(CURTIME(), "08:00")';
-const myDate = 'ADDTIME(NOW(), "08:00")';
-
 class DatabaseTool {
   static insertPageVisit(pageId) {
     if (!developmentMode) {
-      const sqlQuery = `INSERT INTO visits (page_id, date, hour) VALUES (${pageId}, CURTIME(), NOW());`;
+      const sqlQuery = `CALL add_page_visit(${pageId})`;
       db.query(sqlQuery, (err, results) => {
         if (err) console.error(err);
       });
-    } else console.log(`Dev Mode: Page ID ${pageId} visited`);
+    } else console.log(`Dev Mode: Page ID (${pageId}) visited`);
   }
 
   static insertSocialsVisit(linkId) {
     if (!developmentMode) {
-      const sqlQuery = `INSERT INTO link_clicks (link_id, date, hour) VALUES (${linkId}, CURTIME(), NOW());`;
+      const sqlQuery = `CALL add_link_visit(${linkId})`;
       db.query(sqlQuery, (err, results) => {
         if (err) console.error(err);
       });
-    } else console.log(`Dev Mode: Social ID ${linkId} visited`);
+    } else console.log(`Dev Mode: Social ID (${linkId}) visited`);
   }
 }
 
