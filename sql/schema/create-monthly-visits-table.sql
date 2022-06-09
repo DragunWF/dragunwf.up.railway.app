75d49b9d83108a89c5cc6873dcb87670e7f1f3a1
DragonWF <plarisanmarc@gmail.com>
2022-06-09 20:09:57 +0800

    Changed date column type from DATE to VARCHAR(10)

--- sql/schema/create-monthly-visits-table.sql
+++ sql/schema/create-monthly-visits-table.sql
@@ -2,5 +2,5 @@ CREATE TABLE `dragunwf_app_db`.`monthly_visits` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `page_visits` INT NOT NULL,
   `link_visits` INT NOT NULL,
-  `date` DATE NOT NULL,
+  `date` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`id`));
