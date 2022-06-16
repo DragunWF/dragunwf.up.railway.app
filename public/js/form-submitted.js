756f14cec84f22fb02d0196db0adea5afa4e743f
dragunwf <plarisanmarc@gmail.com>
2022-06-16 21:19:50 +0800

    Implemented main functionality and dynamic seconds text for #seconds

--- public/js/form-submitted.js
+++ public/js/form-submitted.js
@@ -1,5 +1,21 @@
 $(document).ready(() => {
-  setTimeout(function () {
-    window.location = "/contact/form";
-  }, 5000);
+  const secondsTag = $("#seconds");
+  let secondsBeforeRedirect = 5;
+
+  function updateSeconds() {
+    secondsTag.text(secondsBeforeRedirect.toString());
+    if (secondsBeforeRedirect > 0) secondsBeforeRedirect--;
+    setTimeout(() => {
+      updateSeconds();
+    }, 1000);
+  }
+
+  function startRedirectTimer() {
+    setTimeout(() => {
+      window.location = "/contact/form";
+    }, 5500);
+    updateSeconds();
+  }
+
+  startRedirectTimer();
 });
