dfddf3fc7f545f74307cfd5dc2328e8cd421f67d
dragunwf <plarisanmarc@gmail.com>
2022-06-16 20:34:49 +0800

    Implemented main form submission action functionality

--- utils/form-bot.js
+++ utils/form-bot.js
@@ -2,7 +2,8 @@ import "dotenv/config";
 import Discord from "discord.js";
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const mailReceiverChannelId = "";
+const messageEmbedColor = "#50C878"; // Emerald Green
+const mailReceiverChannelId = "986967568453615616";
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -11,6 +12,23 @@ client.on("ready", () => {
   });
 });
 
-function sendMessageForm(formBody) {}
+function createEmbedMessage(author, message) {
+  const embed = new Discord.MessageEmbed()
+    .setColor(messageEmbedColor)
+    .setTitle("Message Form Submission")
+    .setAuthor({ name: author })
+    .setDescription(message)
+    .setFooter({ text: "dragunwf.herokuapp.com/contact/form" })
+    .setTimestamp();
+  return embed;
+}
+
+function sendMessageForm(formBody) {
+  const channel = client.channels.cache.get(mailReceiverChannelId);
+  const mailEmbedMessage = createEmbedMessage(formBody.name, formBody.message);
+  channel.send({ embeds: [mailEmbedMessage] });
+}
+
+client.login(process.env.BOT_TOKEN);
 
 export default sendMessageForm;
