eee7aa12a65afbb95ac8c41be859842f0f28317b
dragunwf <plarisanmarc@gmail.com>
2022-06-16 20:41:17 +0800

    Implemented form bot calls

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,7 @@
 import express from "express";
 import DatabaseTool from "../utils/db-tool.js";
 import InfoTool from "../utils/info-tool.js";
+import sendMessageForm from "../utils/form-bot.js";
 
 const contactRouter = express.Router();
 
@@ -12,6 +13,11 @@ contactRouter.get("/", (req, res) => {
 contactRouter.get("/form", (req, res) => {
   res.render("message-form");
   DatabaseTool.insertPageVisit(InfoTool.getPageId("messageForm"));
+});
+
+contactRouter.post("/form", (req, res) => {
+  sendMessageForm(req.body);
+  redirect("")
 });
 
 export default contactRouter;
