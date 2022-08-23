4e1069dc385e815b9e8b30a7aed2f9f248ef2898
dragunwf <plarisanmarc@gmail.com>
2022-08-23 17:00:17 +0800

    Added codewars stat fetching

--- utils/problems-solved-tracker.js
+++ utils/problems-solved-tracker.js
@@ -1,6 +1,23 @@
+import fetch from "node-fetch";
 import cheerio from "cheerio";
 import axios from "axios";
 
 class ProblemsSolvedTracker {
-    
+  static #username = "DragunWF";
+
+  static async getTotalProblemsSolved() {
+    const codewarsStats = await this.#getCodewarsStats();
+    const codechefStats = null;
+    const codeforcesStats = null;
+  }
+
+  static async #getCodewarsStats() {
+    const response = await fetch(
+      `https://www.codewars.com/api/v1/users/${username}`
+    );
+    const stats = await response.json();
+    return stats.codeChallenges.totalCompleted;
+  }
 }
+
+export default ProblemsSolvedTracker;
