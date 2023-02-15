31fec4a09f5a1e0f406e7494823c595358fea6a3
Marc Plarisan <plarisanmarc@gmail.com>
2023-02-15 17:12:13 +0800

    Update formBot.js Apply object oriented structure

--- utils/formBot.js
+++ utils/formBot.js
@@ -1,68 +1,84 @@
 import "dotenv/config";
 import Discord from "discord.js";
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const messageEmbedColor = "#50C878"; // Emerald Green
-const testChannelId = "986967568453615616";
-const mailReceiverChannelIds = [testChannelId, "987014510395142154"];
-
-client.on("ready", () => {
-  console.log(`Bot Online: Logged in as ${client.user.tag}`);
-  client.user.setActivity(`DragunWF's Mails`, {
-    type: "WATCHING",
+class FormBot {
+  static #client = new Discord.Client({
+    intents: ["GUILDS", "GUILD_MESSAGES"],
   });
-});
+  static #embedMessageColor = "#50C878"; // Emerald Green
+  static #testChannelId = "986967568453615616";
+  static #mailDestinationChannelIds = [
+    this.#testChannelId,
+    "987014510395142154",
+  ];
 
-function createEmbedMessage(author, message) {
-  if (!author || !message) {
-    throw new Error("Missing arguments!");
+  static onReady() {
+    this.#client.on("ready", () => {
+      console.log(`Bot Online: Logged in as ${this.#client.user.tag}`);
+      this.#client.user.setActivity(`DragunWF's Mails`, {
+        type: "WATCHING",
+      });
+    });
+    this.#client.login(process.env.BOT_TOKEN);
   }
 
-  const descriptionContent = `**Author:**\n${author}\n\n**Message:**\n${message}`;
-  const embed = new Discord.MessageEmbed()
-    .setColor(messageEmbedColor)
-    .setTitle("Message Form Submission")
-    .setDescription(descriptionContent)
-    .setFooter({ text: "dragunwf.up.railway.app" })
-    .setTimestamp();
-  return embed;
-}
+  static sendMessageForm(formBody) {
+    if (!formBody) {
+      throw new Error("Missing argument!");
+    }
 
-function sendMessageToChannel(channelId, formBody) {
-  if (!channelId || !formBody) {
-    throw new Error("Missing arguments!");
+    const validMessageForm = this.#validateMessageForm(formBody);
+    if (validMessageForm) {
+      for (let channelId of this.#mailDestinationChannelIds) {
+        this.#sendMessageToChannel(channelId, formBody);
+      }
+    } else {
+      this.#sendMessageToChannel(this.#testChannelId, formBody);
+    }
   }
 
-  const mailEmbedMessage = createEmbedMessage(formBody.name, formBody.message);
-  const channel = client.channels.cache.get(channelId);
-  channel.send({ embeds: [mailEmbedMessage] });
-}
+  static #createEmbedMessage(author, message) {
+    if (!author || !message) {
+      throw new Error("Missing arguments!");
+    }
 
-function validateMessageForm(formBody) {
-  if (!formBody) {
-    throw new Error("Missing argument!");
+    const descriptionContent = `**Author:**\n${author}\n\n**Message:**\n${message}`;
+    const embed = new Discord.MessageEmbed()
+      .setColor(this.#embedMessageColor)
+      .setTitle("Message Form Submission")
+      .setDescription(descriptionContent)
+      .setFooter({ text: "dragunwf.up.railway.app" })
+      .setTimestamp();
+    return embed;
   }
 
-  const message = formBody.message.toUpperCase();
-  const author = formBody.name.toUpperCase();
-  return (
-    !author.includes(process.env.SECRET_WORD) &&
-    !message.includes(process.env.SECRET_WORD)
-  );
-}
+  static #sendMessageToChannel(channelId, formBody) {
+    if (!channelId || !formBody) {
+      throw new Error("Missing arguments!");
+    }
 
-function sendMessageForm(formBody) {
-  if (!formBody) {
-    throw new Error("Missing argument!");
+    const mailEmbedMessage = this.#createEmbedMessage(
+      formBody.name,
+      formBody.message
+    );
+    const channel = this.#client.channels.cache.get(channelId);
+    channel.send({ embeds: [mailEmbedMessage] });
   }
 
-  const validMessageForm = validateMessageForm(formBody);
-  if (validMessageForm)
-    for (let channelId of mailReceiverChannelIds)
-      sendMessageToChannel(channelId, formBody);
-  else sendMessageToChannel(testChannelId, formBody);
+  static #validateMessageForm(formBody) {
+    if (!formBody) {
+      throw new Error("Missing argument!");
+    }
+
+    const message = formBody.message.toUpperCase();
+    const author = formBody.name.toUpperCase();
+    return (
+      !author.includes(process.env.SECRET_WORD) &&
+      !message.includes(process.env.SECRET_WORD)
+    );
+  }
 }
 
-client.login(process.env.BOT_TOKEN);
+FormBot.onReady();
 
-export default sendMessageForm;
+export default FormBot;
